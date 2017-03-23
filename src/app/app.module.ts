0775273da7cccd5f6eadf584c2664bf5049bfa20
clayton.passos <clayton.passos@elotech.com.br>
2017-03-23 11:22:15 -0300

    remove script do .angular-cli.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,6 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule
   ],
   providers: [
-    appRoutingProviders,
     AUTH_PROVIDERS,
     Auth,
     AuthGuard
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ declare var Auth0Lock: any;
 @Injectable()
 export class Auth {
   // Configure Auth0
-  lock = new Auth0Lock('IzEMdB5wOaeEkQBZRyAIkUOgWJjrL199', 'netstart.auth0.com', {});
+  lock = new Auth0Lock('IzEMdB5wOaeEkQBZRyAIkUOgWJjrL199', 'netstart.auth0.com', { });
 
   constructor() {
     // Add callback for lock `authenticated` event
